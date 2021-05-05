7f1bdd08b04dfd63cee38e07b8d6ec19ce681d6d
Griffin <griffinstorback@gmail.com>
2021-05-04 17:27:42 -0700

    Initial Commit

---
+++ raywenderlich-practice-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  raywenderlich-practice-project
+//
+//  Created by Griffin Storback on 2021-05-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ raywenderlich-practice-projectTests/raywenderlich_practice_projectTests.swift
@@ -0,0 +1,33 @@
+//
+//  raywenderlich_practice_projectTests.swift
+//  raywenderlich-practice-projectTests
+//
+//  Created by Griffin Storback on 2021-05-04.
+//
+
+import XCTest
+@testable import raywenderlich_practice_project
+
+class raywenderlich_practice_projectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
